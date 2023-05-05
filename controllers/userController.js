0db4776559ad2767b65d629692567d6691ac709a
Mahad444 <mahadsaid95@icloud.com>
2023-05-05 23:23:38 +0300

    changing the controllers of the application

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,116 +1,109 @@
-const userModel = require("../models/UserModels");
-const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const Doctor = require("../models/doctorModel");
+const Appointment = require("../models/appointmentModel");
+
+const getuser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+    return res.send(user);
+  } catch (error) {
+    res.status(500).send("Unable to get user");
+  }
+};
+
+const getallusers = async (req, res) => {
+  try {
+    const users = await User.find()
+      .find({ _id: { $ne: req.locals } })
+      .select("-password");
+    return res.send(users);
+  } catch (error) {
+    res.status(500).send("Unable to get all users");
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const emailPresent = await User.findOne({ email: req.body.email });
+    if (!emailPresent) {
+      return res.status(400).send("Incorrect credentials");
+    }
+    const verifyPass = await bcrypt.compare(
+      req.body.password,
+      emailPresent.password
+    );
+    if (!verifyPass) {
+      return res.status(400).send("Incorrect credentials");
+    }
+    const token = jwt.sign(
+      { userId: emailPresent._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "2 days",
+      }
+    );
+    return res.status(201).send({ msg: "User logged in successfully", token });
+  } catch (error) {
+    res.status(500).send("Unable to login user");
+  }
+};
+
+const register = async (req, res) => {
+  try {
+    const emailPresent = await User.findOne({ email: req.body.email });
+    if (emailPresent) {
+      return res.status(400).send("Email already exists");
+    }
+    const hashedPass = await bcrypt.hash(req.body.password, 10);
+    const user = await User({ ...req.body, password: hashedPass });
+    const result = await user.save();
+    if (!result) {
+      return res.status(500).send("Unable to register user");
+    }
+    return res.status(201).send("User registered successfully");
+  } catch (error) {
+    res.status(500).send("Unable to register user");
+  }
+};
+
+const updateprofile = async (req, res) => {
+  try {
+    const hashedPass = await bcrypt.hash(req.body.password, 10);
+    const result = await User.findByIdAndUpdate(
+      { _id: req.locals },
+      { ...req.body, password: hashedPass }
+    );
+    if (!result) {
+      return res.status(500).send("Unable to update user");
+    }
+    return res.status(201).send("User updated successfully");
+  } catch (error) {
+    res.status(500).send("Unable to update user");
+  }
+};
+
+const deleteuser = async (req, res) => {
+  try {
+    const result = await User.findByIdAndDelete(req.body.userId);
+    const removeDoc = await Doctor.findOneAndDelete({
+      userId: req.body.userId,
+    });
+    const removeAppoint = await Appointment.findOneAndDelete({
+      userId: req.body.userId,
+    });
+    return res.send("User deleted successfully");
+  } catch (error) {
+    res.status(500).send("Unable to delete user");
+  }
+};
 
 module.exports = {
-  // Register a user
-  RegisterController: async (req, res, next) => {
-    try {
-      // Check if user exists
-      const existingUser = await userModel.findOne({ email: req.body.email });
-      if (existingUser) {
-        res
-          .status(200)
-          .json({ success: false, message: "User already exists" });
-      }
-      const password = req.body.password;
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-      req.body.password = hashedPassword;
-
-      const user = await userModel(req.body);
-      await user.save();
-      res
-        .status(200)
-        .json({ success: true, message: "User created successfully" });
-    } catch (err) {
-      res
-        .status(500)
-        .json({
-          success: false,
-          message: `Register Controller ${err.message}`,
-        });
-    }
-  },
-  // Login a user
-  LoginController: async (req, res, next) => {
-    try {
-      //checking if user is available
-
-      const user = await userModel.findOne({ email: req.body.email });
-      if (!user) {
-        return res
-          .status(200)
-          .send({ message: "user not found", success: false });
-      }
-      const password = req.body.password;
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-      const isMatch = await bcrypt.compare(password, hashedPassword);
-      if (!isMatch) {
-        return res
-          .status(200)
-          .send({ message: "Invlid Email or Password", success: false });
-      }
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "9d",
-      });
-      res.status(200).send({ message: "Login Success", success: true, token });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({ message: `Error in Login CTRL ${error.message}` });
-    }
-
-    //     const isMatch = await bcrypt.compare(password, user.password);
-    //     if (!isMatch) { return res.status(200).send({ message: "Invlid Email or Password", success: false });
-    //     }
-    //     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    //       expiresIn: "1d",
-    //     });
-    //     res.status(200).send({ message: "Login Success", success: true, token });
-    //   } catch (error) {
-    //     console.log(error);
-    //     res.status(500).send({ message: `Error in Login CTRL ${error.message}` });
-    //   }
-  },
-  // Get a user
-  AuthController: async (req, res) => {
-    try {
-      const user = await userModel.findOne({ _id: req.body.userId });
-      if (!user) {
-        return res
-          .status(200)
-          .send({ message: "user not found", success: false });
-      }
-      // res.status(200).send({ message: "User Found", success: true, user });
-      else {
-        res.status(200).send({
-          success: true,
-          data: {
-            name: user.name,
-            email: user.email,
-          },
-        });
-      }
-    } catch (error) {
-      console.log(error);
-      res
-        .status(500)
-        .send({ message: `Error in Login CTRL`, success: false, error });
-    }
-  },
+  getuser,
+  getallusers,
+  login,
+  register,
+  updateprofile,
+  deleteuser,
 };
-//     try {
-//         const user = await userModel.find({email: req.body.email});
-//         if (!user) { res.status(200).json({success: false, message: 'User does not exist'});
-//     }
-//         const isMatch = await bcrypt.compare(req.body.password, user.password);
-//         if (!isMatch) { res.status(200).json({success: false, message: 'Invalid email or password'});
-//         }
-//         const token = jwt.sign({id: user.__id}, process.env.JWT_SECRET, {expiresIn: '1d'});
-//         res.status(200).json({success: true, token,message: 'User logged in successfully'});
-//     }
-//     catch (err) {
-//         res.status(500).json({success: false,message: `Login Error in Controller ${err.message}`});
-//     }
-// }
