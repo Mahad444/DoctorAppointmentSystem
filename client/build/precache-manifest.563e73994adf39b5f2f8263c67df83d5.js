28ba0704a86eb9a2a0cae90562533d6839b81c76
Mahad444 <mahadsaid95@icloud.com>
2023-05-28 22:48:10 +0300

    changing my images and herocircles

---
+++ client/build/precache-manifest.563e73994adf39b5f2f8263c67df83d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e6474d33ad299e82bf6",
+    "url": "/static/css/main.62ad26b6.chunk.css"
+  },
+  {
+    "revision": "2e6474d33ad299e82bf6",
+    "url": "/static/js/main.2e6474d3.chunk.js"
+  },
+  {
+    "revision": "3af03192f83efab4825f",
+    "url": "/static/js/1.3af03192.chunk.js"
+  },
+  {
+    "revision": "52abe07b7c019512e5bc",
+    "url": "/static/css/2.a06f7631.chunk.css"
+  },
+  {
+    "revision": "52abe07b7c019512e5bc",
+    "url": "/static/js/2.52abe07b.chunk.js"
+  },
+  {
+    "revision": "4b3cdeaebd06d36ecaa0",
+    "url": "/static/js/runtime~main.4b3cdeae.js"
+  },
+  {
+    "revision": "201493517c7c30338f050214de593fb9",
+    "url": "/index.html"
+  }
+];
